781dbb7817e9aa4caa2fd12921a4cce296eab35e
tvThejohasshen <156106823+tvThejohasshen@users.noreply.github.com>
2025-04-25 15:30:55 +0530

    update

--- Login/main.js
+++ Login/main.js
@@ -1,8 +1,8 @@
 import { UserManager } from 'https://cdn.skypack.dev/oidc-client-ts';
 
 const cognitoAuthConfig = {
-    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_aOUMlhBgb",
-    client_id: "1b7iih9kge05ojlvjta9najefs",
+    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_mjcv7jdTm",
+    client_id: "7ltlrjvtav1jkoqjrolp0j7b01",
     redirect_uri: "https://www.techsg.click",
     response_type: "code",
     scope: "phone openid email"
@@ -14,8 +14,8 @@ export const userManager = new UserManager({
 });
 
 export async function signOutRedirect () {
-    const clientId = "1b7iih9kge05ojlvjta9najefs";
-    const logoutUri = "https://www.techsg.click"; // Make sure to replace with your actual logout URI
-    const cognitoDomain = "https://us-east-1aoumlhbgb.auth.us-east-1.amazoncognito.com";
+    const clientId = "7ltlrjvtav1jkoqjrolp0j7b01";
+    const logoutUri = "https://www.techsg.click";
+    const cognitoDomain = "https://us-east-1mjcv7jdtm.auth.us-east-1.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 };
